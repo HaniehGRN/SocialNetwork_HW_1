c210ec70db77768a9424bc5ea7ed52c17dabef0b
HaniehGRN <gorganihanieh@gmail.com>
2025-12-08 14:33:59 +0330

    code graph class - square lattice method

--- Q1/generate_graph.py
+++ Q1/generate_graph.py
@@ -9,7 +9,14 @@ import numpy as np
 #--------------------- define graph class ---------------------
 
 class Graph:
+
     def ring_lattice(N, k=2):
         G = nx.watts_strogatz_graph(N, k, p=0) # p=0 not to rewire
         pos = nx.circular_layout(G)
+        return G, pos
+
+    @staticmethod
+    def square_Lattice(Lx, Ly):
+        G = nx.grid_2d_graph(Lx, Ly, periodic=True)
+        pos = {(i, j): np.array([i, j]) for i, j in G.nodes()}
         return G, pos
