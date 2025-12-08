129dc9492fd99331f8151168ee1f266a1f92b098
HaniehGRN <gorganihanieh@gmail.com>
2025-12-08 14:33:16 +0330

    code graph class - ring lattice method

--- Q1/generate_graph.py
+++ Q1/generate_graph.py
@@ -5,3 +5,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 import numpy as np
+
+#--------------------- define graph class ---------------------
+
+class Graph:
+    def ring_lattice(N, k=2):
+        G = nx.watts_strogatz_graph(N, k, p=0) # p=0 not to rewire
+        pos = nx.circular_layout(G)
+        return G, pos
