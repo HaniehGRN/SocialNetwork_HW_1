884102dd15306a75affd7d3550465df6eb6707dd
HaniehGRN <gorganihanieh@gmail.com>
2025-12-09 14:31:38 +0330

    code remove giant component from graph & plot giant component % other components seperately

--- Q3/Q2_main_code.py
+++ Q3/Q2_main_code.py
@@ -31,7 +31,8 @@ def identify_connected_components(G):
     return connected_components
 
 def get_giant_component(G):
-    giant_component_nodes = max(nx.connected_components(G), key=len)  # Get the iterator of all connected components (sets of nodes)
+    giant_component_nodes = max(nx.connected_components(G), key=len) # Get the iterator of all connected components (sets of nodes)
+    # print(giant_component_nodes)
     giant_component = G.subgraph(giant_component_nodes).copy()
     return giant_component
 
@@ -42,16 +43,49 @@ def get_relative_giant_component_size(giant_component_size, N):
     S = NG / N
     return S
 
-def get_average_size_non_giant_clusters(giant_component, connected_components):
-    print(giant_component, connected_components)
+def get_average_size_non_giant_clusters(G, pos, giant_component, connected_components):
+    # for connected_component in connected_components:
+    #     print(connected_component)
+    temp = [sorted(list(connected_component)) for connected_component in connected_components]
+    print(giant_component.nodes())
+    print(temp)
+    temp2 = temp.remove(sorted(giant_component.nodes()))
+    print(connected_components)
+    temp2 = G.subgraph(temp2)
+    plt.figure(figsize=(9, 4))
+    plt.subplot(1, 2, 1)
+    nx.draw(
+        giant_component,
+        pos,
+        with_labels=False,  # Don't show node labels for clarity
+        node_size=50,  # Smaller nodes
+        width=0.5,  # Thinner edges
+        edge_color="black",
+        node_color="red",
+    )
+    plt.subplot(1, 2, 2)
+    nx.draw(
+        temp2,
+        pos,
+        with_labels=False,  # Don't show node labels for clarity
+        node_size=50,  # Smaller nodes
+        width=0.5,  # Thinner edges
+        edge_color="black",
+        node_color="red",
+    )
+    plt.tight_layout()
+    plt.show()
+    # print(temp)
+    # print(temp2)
 
 instance_graph = Graph()
 # for k in range(average_degree_lower_bound, non_critical_region_upper_bound, step_size_non_critical_region):
-k = 0.6
+k = 3.6
 
 # plot the graph
 
 G, pos = Graph.random_network(N, k)
+plt1 = plt.figure(figsize=(9, 4))
 nx.draw(
     G,
     pos,
@@ -61,17 +95,10 @@ nx.draw(
     edge_color="gray",
     node_color="skyblue",
 )
-# plt.title(f"Erdos-Rényi Random Graph")
-# plt.show()
-
 connected_components = identify_connected_components(G)
 giant_component = get_giant_component(G)
 giant_component_size = giant_component.number_of_nodes()
 S = get_relative_giant_component_size(giant_component_size, N)
-# get_average_size_non_giant_clusters(giant_component, connected_components)
-# print(S)
-print(connected_components, giant_component)
-
 nx.draw(
     giant_component,
     pos,
@@ -81,11 +108,12 @@ nx.draw(
     edge_color="black",
     node_color="red",
 )
-# plt.title(f"Erdos-Rényi Random Graph")
-plt.show()
-
-
-
+# plt1.title(f"Erdos-Rényi Random Graph")
+plt1.show()
+get_average_size_non_giant_clusters(G, pos, giant_component, connected_components)
+# print(S)
+# print(nx.nodes(connected_components), nx.nodes(giant_component))
+#
 
 
 
