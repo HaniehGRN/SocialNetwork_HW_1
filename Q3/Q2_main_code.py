24a2834c74edd55ff115f5495b18da9aa48508be
HaniehGRN <gorganihanieh@gmail.com>
2025-12-09 18:42:32 +0330

    code plot S & <s> for both sub-critical & critical regions

--- Q3/Q2_main_code.py
+++ Q3/Q2_main_code.py
@@ -15,10 +15,14 @@ average_degree_lower_bound = 0   # <k> lower bound
 average_degree_upper_bound = 5   # <k> upper bound
 step_size_non_critical_region = 0.1
 step_size_critical_region = 0.02
-# non_critical_region_lower_bound = 0
 non_critical_region_upper_bound = 0.8
-# critical_region_lower_bound = 0.8
 critical_region_upper_bound = 1.2
+
+#--------------------- define variables ---------------------
+
+average_degree = []
+S_relative_giant_component_size = []
+s_average_size_small_clusters = []
 
 #--------------------- define functions ---------------------
 
@@ -32,18 +36,17 @@ def identify_connected_components(G):
 
 def get_giant_component(G):
     giant_component_nodes = max(nx.connected_components(G), key=len) # Get the iterator of all connected components (sets of nodes)
-    # print(giant_component_nodes)
     giant_component = G.subgraph(giant_component_nodes).copy()
-    return giant_component
+    giant_component_size = giant_component.number_of_nodes()
+    return giant_component, giant_component_size
 
 def get_relative_giant_component_size(giant_component_size, N):
-    # giant_component = get_giant_component(G)
-    # NG = giant_component.number_of_nodes()
     NG = giant_component_size
     S = NG / N
     return S
 
-def get_small_clusters_subgraph(G, giant_component):
+def get_small_clusters_subgraph(G):
+    giant_component, giant_component_size = get_giant_component(G)
     all_nodes = set(G.nodes)
     remaining_nodes = all_nodes.difference(giant_component)
     remaining_nodes_subgraph = G.subgraph(remaining_nodes)
@@ -103,22 +106,47 @@ def plot_components(G, pos, giant_component, small_clusters):
 def get_average_size_small_clusters(small_clusters_subgraph):
     small_clusters = identify_connected_components(small_clusters_subgraph)
     average_size_small_clusters = np.average([len(small_cluster) for small_cluster in small_clusters])
-    print("Average size of small clusters: ", average_size_small_clusters)
+    # print("Average size of small clusters: ", average_size_small_clusters)
+    return average_size_small_clusters
+
+def plot_S_and_s(S, s, k):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6), sharex=True)
+    ax1.plot(k, S, color="blue")
+    ax1.set_title("The Order Parameter S=NG/N")
+    ax1.set_ylabel("S")
+    ax2.plot(k, s, color='red')
+    ax2.set_title("The Average Size Of Isolated Clusters <s>")
+    ax2.set_ylabel("s")
+    fig.supxlabel("<k>")
+    fig.subplots_adjust(hspace=0)
+    plt.tight_layout(rect=[0, 0.05, 1, 0.95])
+    plt.show()
+
+def get_S_and_s(instance_graph, k):
+    average_degree.append(k)
+    S = []
+    s = []
+    for i in range(5):  # must be 50
+        G, pos = instance_graph.random_network(N, k)
+        connected_components = identify_connected_components(G)
+        giant_component, giant_component_size = get_giant_component(G)
+        small_clusters_subgraph = get_small_clusters_subgraph(G)
+        S.append(get_relative_giant_component_size(giant_component_size, N))
+        s.append(get_average_size_small_clusters(small_clusters_subgraph))
+
+    S_relative_giant_component_size.append(np.average(S))
+    s_average_size_small_clusters.append(np.average(s))
 
 instance_graph = Graph()
-# for k in range(average_degree_lower_bound, non_critical_region_upper_bound, step_size_non_critical_region):
-k = 4
+for k in np.arange(average_degree_lower_bound, non_critical_region_upper_bound, step_size_non_critical_region):
+    get_S_and_s(instance_graph, k)
+for k in np.arange(non_critical_region_upper_bound, critical_region_upper_bound, step_size_critical_region):
+    get_S_and_s(instance_graph, k)
+print(S_relative_giant_component_size)
+print(s_average_size_small_clusters)
+print(average_degree)
+plot_S_and_s(S_relative_giant_component_size, s_average_size_small_clusters, average_degree)
 
-# plot the graph
-g
-for i in range(50):
-    G, pos = Graph.random_network(N, k)
-    connected_components = identify_connected_components(G)
-    giant_component = get_giant_component(G)
-    giant_component_size = giant_component.number_of_nodes()
-    S_relative_giant_component_size = get_relative_giant_component_size(giant_component_size, N)
-    small_clusters_subgraph = get_small_clusters_subgraph(G, giant_component)
-    s_average_size_small_clusters = get_average_size_small_clusters(small_clusters_subgraph)
 
 # plot_components(G, pos, giant_component, small_clusters)
 
