412aa9f9014c48d872cbd722f411eaa2b5cbee75
HaniehGRN <gorganihanieh@gmail.com>
2025-12-10 19:57:42 +0330

    code generate RG - Q2 part(a)

--- Q2/Q2_part_a.py
+++ Q2/Q2_part_a.py
@@ -1,30 +1,42 @@
-#--------------------- import libraries ---------------------
+# --------------------- import libraries ---------------------
 
 import networkx as nx
 import numpy as np
 import re
 
-#--------------------- generate graph ---------------------
+from networkx.classes import edges
+
+
+# --------------------- generate graph ---------------------
 
 def generate_pattern(b):
-    source_set = [ (r'^' + (i * '0') + ((b - i) * '.')) for i in range(0, b) ]
-    destination_set = [ (r'' + ((b - i) * '.') + (i * '1')) for i in range(0, b)]
-    print(source_set)
-    print(destination_set)
-    return source_set, destination_set
+    source_pattern = [(r'^' + (i * '0') + ((b - i) * '.')) for i in range(0, b)]
+    destination_pattern = [(r'' + (i * '.') + ((b - i) * '1')) for i in range(0, b)]
+    return source_pattern, destination_pattern
 
-def detect_matching(source_set, destination_set, b, nodes_list):
-    destination_patterns_mask = [[ node for node in nodes_list if re.match(destination_pattern, node)] for destination_pattern in destination_set]
-    source_patterns_mask = [[ node for node in nodes_list if re.match(source_pattern, node)] for source_pattern in source_set]
-    print(destination_patterns_mask)
-    print(source_patterns_mask)
+
+def detect_matching(source_pattern, destination_pattern, nodes_list):
+    destination_set = [[node for node in nodes_list if re.match(pattern, node)] for
+                                 pattern in destination_pattern]
+    source_set = [[node for node in nodes_list if re.match(pattern, node)] for pattern in
+                            source_pattern]
+    return destination_set, source_set
 
 
 def generate_network(N, b):
-    nodes_list = [ f'{i:0{b}b}' for i in range(0, N) ]
-    return nodes_list
+    nodes_list = [f'{i:0{b}b}' for i in range(0, N)]
+    source_pattern, destination_pattern = generate_pattern(b)
+    source_set, destination_set = detect_matching(source_pattern, destination_pattern, nodes_list)
+    edges_list = []
+    for i in range(b):
+        for source_node in source_set[i]:
+            for destination_node in destination_set[i]:
+                edges_list.append([source_node, destination_node])
 
-nodes_list = generate_network(16, 4)
-print(nodes_list)
-source_set, destination_set = generate_pattern(4)
-detect_matching(source_set, destination_set, 4, nodes_list)
+    return nodes_list, edges_list
+
+b = 3
+N = 8
+nodes_list, edges_list = generate_network(N, b)
+# print(nodes_list)
+# print(edges_list)
