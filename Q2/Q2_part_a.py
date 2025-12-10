47381985b6df64b4773c77f95b820dbc50b7d5f9
HaniehGRN <gorganihanieh@gmail.com>
2025-12-10 18:05:51 +0330

    code generate pattern - Q2 part (a)

--- Q2/Q2_part_a.py
+++ Q2/Q2_part_a.py
@@ -3,4 +3,19 @@
 import networkx as nx
 import numpy as np
 
-#--------------------- import libraries ---------------------
+#--------------------- generate graph ---------------------
+
+def generate_pattern(b):
+    source_set = [((i * '0') + ((b - i) * 'X')) for i in range(0, b)]
+    destination_set = [(((b - i) * 'X') + (i * '1')) for i in range(0, b)]
+    return source_set, destination_set
+
+# def detect_matching(source_set, destination_set, b):
+#     for i in range(0, b):
+#
+#
+#
+# def generate_network(N, b):
+#
+#     nodes_list = [ f'{i:0{b}b}' for i in range(0, N) ]
+#     edges_list = [ for in range(0, N) ]
